735dd34ae18a4294cd13d6ee77e51165409f98e9
CN <dgxa9999@gmail.com>
2024-02-26 17:07:06 +0700

    1

---
+++ src/components/dropdown-header/index.jsx
@@ -0,0 +1,72 @@
+import css from "src/components/dropdown-header/dropdown-header.module.scss";
+import React from "react";
+import { FaArrowRightFromBracket } from "react-icons/fa6";
+
+export const dropdownHeaderAlignEnum = {
+  left: "left",
+  center: "center",
+  right: "right",
+};
+function DropdownHeader(props) {
+  const { header, list, align } = props;
+
+  const renderBorder = function (value) {
+    return value ? "border-b-1" : "";
+  };
+  const renderIcon = function (value) {
+    return value ? <FaArrowRightFromBracket /> : "";
+  };
+  const renderListItem = function () {
+    return list.map((item, index) => {
+      return (
+        <div
+          key={item.id}
+          className={`${
+            css["headerMenu__item"]
+          } hover-p flex align-center justify-between ${renderBorder(
+            item.borderBottom
+          )}`}
+        >
+          <div>{item.content}</div>
+          {renderIcon(item.icon)}
+        </div>
+      );
+    });
+  };
+  const renderAlign = function (value) {
+    switch (value) {
+      case dropdownHeaderAlignEnum.left:
+        return { left: 0 };
+
+      case dropdownHeaderAlignEnum.right:
+        return { right: 0 };
+
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className={`${css["dropdownHeader"]} py-1`}>
+      <div
+        className={`${css["headerContainer"]} flex align-center justify-center p-3 select-none hover-p`}
+      >
+        {header}
+      </div>
+      <div
+        style={renderAlign(align)}
+        className={`${css["headerMenu"]} py-2 d-0 border-1`}
+      >
+        {renderListItem()}
+      </div>
+    </div>
+  );
+}
+
+DropdownHeader.defaultProps = {
+  header: "test",
+  list: [],
+  align: dropdownHeaderAlignEnum.left,
+};
+
+export default DropdownHeader;
--- src/templates/header-user/index.jsx
+++ src/templates/header-user/index.jsx
@@ -3,8 +3,141 @@ import css from "./header-user.module.scss";
 import { IoIosClose } from "react-icons/io";
 import { NavLink } from "react-router-dom";
 import { HiOutlineDotsHorizontal } from "react-icons/hi";
+import { MdLanguage } from "react-icons/md";
+import { IoSettingsSharp } from "react-icons/io5";
+import { IoIosNotifications } from "react-icons/io";
+import Logo1 from "/src/assets/imgs/logo.svg";
+import DropdownHeader, {
+  dropdownHeaderAlignEnum,
+} from "../../components/dropdown-header";
 
 function Header() {
+  const threeDotMenuList = [
+    {
+      id: 1,
+      content: "Info",
+    },
+    {
+      id: 2,
+      content: "IFO",
+    },
+    {
+      id: 3,
+      content: "Affiliate Program",
+    },
+    {
+      id: 4,
+      content: "Voting",
+      borderBottom: true,
+    },
+    {
+      id: 5,
+      content: "LeaderBoard",
+      borderBottom: true,
+    },
+    {
+      id: 6,
+      content: "Blogs",
+      icon: true,
+    },
+    {
+      id: 6,
+      content: "Docs",
+      icon: true,
+    },
+  ];
+  const nftMenuList = [
+    {
+      id: 1,
+      content: "Overide",
+    },
+    {
+      id: 2,
+      content: "Colection",
+    },
+    {
+      id: 3,
+      content: "Activity",
+    },
+  ];
+  const gameMenuList = [
+    {
+      id: 1,
+      content: "Game Marketplace",
+      icon: true,
+    },
+    {
+      id: 2,
+      content: "Prediction (BETA)",
+    },
+    {
+      id: 3,
+      content: "Lottery",
+    },
+    {
+      id: 4,
+      content: "Bottery (BETA)",
+    },
+  ];
+  const earnList = [
+    {
+      id: 1,
+      content: "Farms",
+    },
+    {
+      id: 2,
+      content: "Cake Staking",
+    },
+    {
+      id: 3,
+      content: "Pool",
+    },
+    {
+      id: 4,
+      content: "Position Manager",
+    },
+    {
+      id: 5,
+      content: "Liquid Staking",
+    },
+    {
+      id: 6,
+      content: "Simple Staking",
+    },
+  ];
+  const tradeMenu = [
+    {
+      id: 1,
+      content: "Swap",
+    },
+    {
+      id: 2,
+      content: "Liquidity",
+    },
+    {
+      id: 3,
+      content: "Pequetual",
+      icon: true,
+    },
+    {
+      id: 4,
+      content: "Bride",
+      icon: true,
+    },
+    {
+      id: 5,
+      content: "Limit (V2)",
+    },
+    {
+      id: 6,
+      content: "Buy Crypto",
+    },
+    {
+      id: 7,
+      content: "Trading Reward",
+    },
+  ];
+
   return (
     <>
       <div className={`${css["header1"]} flex w-100`}>
@@ -34,40 +167,67 @@ function Header() {
           <IoIosClose />
         </div>
       </div>
-      <div className={`${css["header2"]} px-3`}>
-        <div className={`${css["header2__left"]}`}>
-          <div className={`${css["header2__icon"]}`}>
-            <NavLink to="/">
-              <img src={"/src/assets/imgs/header-icon-1.png"} alt="logo" />
-              <img src={"/src/assets/imgs/header-icon-2.png"} alt="logo" />
+      <div
+        className={`${css["header2"]} border-b-1 px-3 flex align-center justify-between`}
+      >
+        <div className={`${css["header2__left"]} flex align-center`}>
+          <div
+            className={`${css["header2__icon"]} flex align-center justify-between`}
+          >
+            <NavLink className={`flex align-center justify-between`} to="/">
+              <img
+                className={`${css["header2__icon__image"]}`}
+                src={Logo1}
+                alt="React Logo"
+              />
+              <img
+                className="d-0"
+                src={"src/assets/imgs/logo2.svg"}
+                alt="logo"
+              />
             </NavLink>
           </div>
-          <div className={`${css["header2__menu"]}`}>
+          <div className={`${css["header2__menu"]} flex align-center ml-3`}>
             <div className={`${css["header2__menu__item"]}`}>
-              <NavLink to="/">Trade</NavLink>
+              <DropdownHeader header={"Trade"} list={tradeMenu} />
             </div>
             <div className={`${css["header2__menu__item"]}`}>
-              <NavLink to="/">Earn</NavLink>
+              <DropdownHeader header={`Earn`} list={earnList} />
             </div>
             <div className={`${css["header2__menu__item"]}`}>
-              <NavLink to="/">Game</NavLink>
+              <DropdownHeader header={`Game`} list={gameMenuList} />
             </div>
             <div className={`${css["header2__menu__item"]}`}>
-              <NavLink to="/">NFT</NavLink>
+              <DropdownHeader header={`NFT`} list={nftMenuList} />
             </div>
-
-            <div className={`${css["header2__menu__item"]}`}>
-              <HiOutlineDotsHorizontal />
+            <div
+              className={`${css["header2__menu__item"]} flex align-center justify-center`}
+            >
+              <DropdownHeader
+                header={<HiOutlineDotsHorizontal />}
+                list={threeDotMenuList}
+              />
             </div>
           </div>
         </div>
-        <div className={`${css["header2__right"]}`}>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className={`${css["header2__right"]} flex`}>
+          <div>
+            <span className={`${css["header2__money__image"]}`}>
+              <img src="src/assets/imgs/pancakeswap-logo.png" />
+            </span>
+            <span>$3.107</span>
+          </div>
+          <div>
+            <MdLanguage />
+          </div>
+          <div>
+            <IoSettingsSharp />
+          </div>
+          <div>
+            <IoIosNotifications />
+          </div>
+          <div>BNB Chain</div>
+          <div>connenct wallet</div>
         </div>
       </div>
     </>
