f986cff50bd4f493d4d2e8a03d409d6a4191e655
CN <dgxa9999@gmail.com>
2024-03-26 17:09:01 +0700

    1

--- src/redux/config-store.js
+++ src/redux/config-store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import paddingTopPage from "./slices/paddingTopPage";
+import headerComponent2 from "./slices/headerComponent2";
 
 export const store = configureStore({
   reducer: {
     paddingTopPage,
+    headerComponent2
   },
 });
---
+++ src/redux/slices/headerComponent2.js
@@ -0,0 +1,19 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+	showMenu: false
+};
+
+const headerComponentSlice = createSlice({
+	name: 'headerComponentSlice',
+	initialState,
+	reducers: {
+		setShowMenu(state, action) {
+			state.paddingTop = action.payload;
+		}
+	},
+});
+
+export const {setShowMenu} = headerComponentSlice.actions;
+export default headerComponentSlice.reducer;
+export const getShowMenu = (state) => state.headerComponent2.showMenu;
